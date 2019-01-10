b8f9f98bf3c7926a9ee7bcf7fe6f7dfd0f93f6fe
Harizo <rajaonarisoazo@gmail.com>
2019-01-10 09:57:34 +0300

    TEST PUSH POPULATION 1

--- application/controllers/api/Site.php
+++ application/controllers/api/Site.php
@@ -3,6 +3,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 //harizo
 // afaka fafana refa ts ilaina
+
+//TEST PUSH
 require APPPATH . '/libraries/REST_Controller.php';
 
 class Site extends REST_Controller {
